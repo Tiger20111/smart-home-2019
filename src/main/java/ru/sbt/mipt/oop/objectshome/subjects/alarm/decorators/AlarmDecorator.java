251660ccd10929b5f9ea8c0a00b10bcf7c423526
Tiger20111 <nikit-proc@mail.ru>
2019-03-21 21:11:58 +0000

    Refactored

--- src/main/java/ru/sbt/mipt/oop/objectshome/subjects/alarm/decorators/AlarmDecorator.java
+++ src/main/java/ru/sbt/mipt/oop/objectshome/subjects/alarm/decorators/AlarmDecorator.java
@@ -3,7 +3,6 @@ package ru.sbt.mipt.oop.objectshome.subjects.alarm.decorators;
 import ru.sbt.mipt.oop.Alarm;
 import ru.sbt.mipt.oop.EventProcessor;
 import ru.sbt.mipt.oop.SmartHome;
-import ru.sbt.mipt.oop.objectshome.subjects.alarm.AlarmState;
 import ru.sbt.mipt.oop.objectshome.subjects.alarm.statesalarm.ActiveAlarmState;
 import ru.sbt.mipt.oop.objectshome.subjects.alarm.statesalarm.AnxiousAlarmState;
 import ru.sbt.mipt.oop.objectshome.subjects.alarm.statesalarm.DeactiveAlarmState;
@@ -20,23 +19,21 @@ public class AlarmDecorator implements EventProcessor {
 
     if (alarm.getState() instanceof AnxiousAlarmState) {
       return;
+    }
 
-      if (alarm.getState() instanceof DeactiveAlarmState) {
-        eventProcessor.processEvent(smartHome, event);
-        return;
+    if (alarm.getState() instanceof DeactiveAlarmState) {
+      eventProcessor.processEvent(smartHome, event);
+      return;
+    }
+
+
+    if (alarm.getState() instanceof ActiveAlarmState) {
+      eventProcessor.processEvent(smartHome, event);
+      CommandHomeType allCommandHome = new CommandHomeType();
+      if (allCommandHome.hasEnventCommandHome(event)) {
+        alarm.turnOnAlert();
       }
-
-
-      if (alarm.getState() instanceof ActiveAlarmState) {
-        eventProcessor.processEvent(smartHome, event);
-        CommandHomeType allCommandHome = new CommandHomeType();
-        if (allCommandHome.hasEnventCommandHome(event)) {
-          alarm.turnOnAlert();
-        }
-        return;
-      }
+      return;
     }
   }
-
-
 }
